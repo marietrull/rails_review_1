6302d9a908e577fa5373d5464fd10dbb29980fb6
marietrull <marietrull@gmail.com>
2018-06-26 14:37:13 -0500

    Added associations

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
+	belongs_to :user
 	# adding validation that says that the length cannot exceed 140 characters
 	validates :content, length: { maximum: 140 }
 end
