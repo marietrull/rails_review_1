9e2594cafa9a36a413fba3acf2e70e6c6addf050
marietrull <marietrull@gmail.com>
2018-06-26 13:26:17 -0500

    Basic 'Hello World' Controller and Route

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+	# hello action that will return a desired string
+	def hello
+		render html: "Hello, World!"
+	end
+
 end
+
+
--- hello_app/config/routes.rb
+++ hello_app/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# standard syntax is root 'controller_name#action_name'
+	# the controller name is application, and the action name is hello
+	root 'application#hello'
 end
